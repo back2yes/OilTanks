dfd2e88ec9266a7d0e1ceb092ca252e2212ac2e3
back2yes <gjylucky@163.com>
2017-12-21 13:17:55 +0800

    WIP project init commit

--- utils/pascal_parser.py
+++ utils/pascal_parser.py
@@ -48,21 +48,65 @@ def parseXML(xml_path):
     return size, classes, bndboxes
 
 
+IMG_EXTENSIONS = [
+    '.jpg', '.JPG', '.jpeg', '.JPEG',
+    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+]
+
+
+def is_image_file(filename):
+    return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
+
+
+def make_dataset(imgdir, xmldir):
+    datalist = []
+    imgdir = os.path.expanduser(imgdir)
+    for target in sorted(os.listdir(imgdir)):
+        imgfn = reformatSlash(os.path.join(imgdir, target))
+        if not is_image_file(imgfn):
+            continue
+        # xmlfn = imgfn.replace('Annotations', 'JPEGImages')[:-4] + '.xml'
+        xmlfn = reformatSlash(os.path.join(xmldir, renameExt(target, 'xml')))
+        xml_data = parseXML(xmlfn)
+        datalist.append((imgfn, xml_data))
+
+    return datalist
+
+def reformatSlash(string):
+    return string.replace('\\', '/')
+
+def renameExt(fn, ext='xml'):
+    return fn.rsplit('/', 1)[-1][:-4] + '.' + ext
+
+
+def pil_loader(path):
+    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
+    with open(path, 'rb') as f:
+        with Image.open(f) as img:
+            return img.convert('RGB')
+
+
 # print(parseXML(xml_data))
 
 class PascalDataset(Dataset):
     def __init__(self, dataroot):
         self.dataroot = dataroot
+        self.datalist = make_dataset()
 
-    def __getitem__(self, item):
+
+    def __getitem__(self, index):
+        imgfn, annot = self.datalist[index]
+
 
 
 if __name__ == '__main__':
-    import glob, os.path
+    import glob
+    import os.path
 
-    for ii, fn in enumerate(sorted(glob.glob(os.path.join('D:/VOCdevkit/VOC2007/Annotations', '*.xml')))):
-        # fn = fn.replace('\\', '/')
-        # with open(fn) as f:
-
-        print(fn)
-        print(parseXML(fn))
+    # for ii, fn in enumerate(sorted(glob.glob(os.path.join('D:/VOCdevkit/VOC2007/Annotations', '*.xml')))):
+    # print(fn)
+    # print(parseXML(fn))
+    xmldir = 'D:/VOCdevkit/VOC2007/Annotations'
+    imgdir = 'D:/VOCdevkit/VOC2007/JPEGImages'
+    for item in make_dataset(imgdir, xmldir):
+        print(item)
